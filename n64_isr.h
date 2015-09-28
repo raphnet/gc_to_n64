d5d3c9467062f5955043af28274b3d5664022157
Raphaël Assénat <raph@raphnet.net>
2015-09-28 21:42:18 +0000

    Removed useless different answer to reset

--- n64_isr.h
+++ n64_isr.h
@@ -13,10 +13,8 @@ extern volatile unsigned char n64_rx_buf[32];
 extern volatile unsigned char n64_tx_buf0[32];
 extern volatile unsigned char n64_tx_buf1[32];
 
-// usually constant, therefore not doublebuffered. Reply to 0x00 (id)
+// usually constant, therefore not doublebuffered. Reply to 0x00 (id) and 0xff (reset)
 extern volatile unsigned char n64_tx_id_reply[3]; // { 0x05, 0x00, 0x02 };
-// reply to 0xff command
-extern volatile unsigned char n64_tx_id2_reply[3]; // { 0x05, 0x00, 0x00 };
 
 // dedicated registers for communicating with interrupt
 extern volatile unsigned char n64_got_command;
